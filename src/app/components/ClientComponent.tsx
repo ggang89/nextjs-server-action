c962e381b6e73b366e2f2d8f5aa5d0903158de3b
ggang89 <ggang89@gmail.com>
2025-02-23 19:22:56 +0900

    별도의 파일을 생성해서 서버액션을 만듦 => 클라이언트 컴포넌트

---
+++ src/app/components/ClientComponent.tsx
@@ -0,0 +1,13 @@
+import { myAction } from '@/app/lib/actions'
+import React from 'react'
+
+const ClientComponent = () => {
+  return (
+    <form action={myAction}>
+      <button type='submit'>Add to Cart</button>
+    </form>
+  
+  )
+}
+
+export default ClientComponent
