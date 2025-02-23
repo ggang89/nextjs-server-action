78612941a969527070c51ca47f82cd03b1d4c33a
ggang89 <ggang89@gmail.com>
2025-02-23 19:06:10 +0900

    비동기 함수 위에 use server지시어 사용=> 비동기함수가 있는 컴포넌트가 서버 컴포넌트임

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
 
-export default function Home() {
+export default function ServerComponent() {
+  async function myAction() {
+    'use server'; // 비동기함수 본문 상단에 지시어 사용
+  }
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
     server    
