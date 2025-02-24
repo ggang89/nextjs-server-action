375c7366c042e8acf51b02f64d1f8391e4c3b8b7
ggang89 <ggang89@gmail.com>
2025-02-24 23:20:53 +0900

    server action으로 새로운 할일 데이터에 추가하기

--- src/app/components/Form.tsx
+++ src/app/components/Form.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import { addTodo } from "../lib/actions";
 
 const Form = () => {
   return (
-    <div>Form</div>
-  )
-}
+    <form action={addTodo} className="flex items-center gap-2">
+      <input
+        type="text"
+        name="title"
+        autoFocus
+        placeholder="새로운 할 일을 생성하세요"
+        className="flex-grow w-full p-1 text-2xl rounded-lg"
+      />
+      <button type="submit">submit</button>
+    </form>
+  );
+};
 
-export default Form
+export default Form;
--- src/app/lib/actions.ts
+++ src/app/lib/actions.ts
@@ -1,5 +1,17 @@
 'use server';
 
-export async function myAction() { 
-  
+import { revalidatePath } from "next/cache";
+
+export async function addTodo(data:FormData) {
+  const title = data.get('title');
+
+  await fetch('http://localhost:3001/todos', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ title, completed: false, userId: 1 }),
+  })
+
+  revalidatePath('/');
 }
